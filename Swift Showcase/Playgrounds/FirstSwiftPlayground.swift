57f0a868764d19642a1c8b3da8e2b6e95ce10fba
Simone Civetta <simonecivetta@gmail.com>
2014-06-03 18:53:15 +0200

    First commit

---
+++ Swift Showcase/Playgrounds/FirstSwiftPlayground.swift
@@ -0,0 +1,15 @@
+//
+//  FirstSwiftPlayground.swift
+//  Swift Showcase
+//
+//  Created by Simone Civetta on 03/06/14.
+//  Copyright (c) 2014 Xebia IT Architects. All rights reserved.
+//
+
+import Foundation
+
+class Toto {
+    func sayHelloTo(name:String) -> String {
+        return "Hello " + name
+    }
+}
---
+++ Swift ShowcaseTests/Swift_ShowcaseTests.swift
@@ -0,0 +1,32 @@
+//
+//  Swift_ShowcaseTests.swift
+//  Swift ShowcaseTests
+//
+//  Created by Simone Civetta on 03/06/14.
+//  Copyright (c) 2014 Xebia IT Architects. All rights reserved.
+//
+
+import XCTest
+
+class Swift_ShowcaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testSayHello() {
+        var x = Toto()
+        XCTAssert(x.sayHelloTo("World") == "Hello World", "Pass")
+    }
+    
+    func testSayHello() {
+        var x = Toto()
+        XCTAssert(x.sayHelloTo("Simone") == "Hello Simone", "Pass")
+    }
+}
